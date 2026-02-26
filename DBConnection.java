e303dd4c213343fb8a4acd6a62e8ec4227a1ccb2
patelkavan197-byte <patelkavan197@gmail.com>
2026-02-26 19:00:44 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/moviebooking",
+            "root",
+            "password");
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
